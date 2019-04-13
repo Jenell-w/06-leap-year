05ddc7bffb15a9a313ce85697d88cda04826a71d
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-04-13 10:54:23 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -5,4 +5,10 @@
 # Don't forget to reach out for help after your own due diligence
 
 def is_leap_year(year):
-    return ''
+    if (year % 4 == 0) and (year % 100 != 0):
+        return True
+    if (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
+        return True
+    else:
+        return False
+    
